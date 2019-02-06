25520390ea78efe81b381bf811c80041677d6e08
serhijko <sp_by@tut.by>
2019-02-06 17:34:48 -0300

    Sign Up with React and Firebase

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <div>
     <ul>
       <li>
-        <Link to={ROUTES.SING_IN}>Sign In</Link>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
       </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const LANDING = '/';
-export const SING_UP = '/signup';
-export const SING_IN = '/signin';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
