82f490a5b06f696e41f9ee912edbc63d259404f9
serhijko <sp_by@tut.by>
2019-03-01 21:01:02 +0300

    Linking Social Logins

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -22,6 +22,16 @@ const INITIAL_STATE = {
   error: null,
 };
 
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+  An account with this E-Mail address already exists.
+  Try to login with this account instead. If you think the
+  account is already used from one of the social logins, try
+  to sign-in with one of them. Afterward, associate your accounts
+  on your personal account page.
+`;
+
 class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
@@ -48,7 +58,7 @@ class SignUpFormBase extends Component {
             email,
             roles,
           })
-            .then(() => {
+          .then(() => {
             this.setState({ ...INITIAL_STATE });
             this.props.history.push(ROUTES.HOME);
           })
@@ -57,6 +67,10 @@ class SignUpFormBase extends Component {
           });
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+        
         this.setState({ error });
       });
     
