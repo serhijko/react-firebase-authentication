ab4126eb6411ad59f67a0c6b13494e026b505ed8
serhijko <sp_by@tut.by>
2019-03-02 15:39:30 +0300

    Get a List from Firebase Realtime Database

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { compose } from 'recompose';
 
 import { withAuthorization, withEmailVerification } from '../Session';
+import { withFirebase } from '../Firebase';
 
 const HomePage = () => (
   <div>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
+
+    <Messages />
   </div>
 );
+
+class MessageBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.messages().on('value', snapshot => {
+      const messageObject = snapshot.val();
+      
+      if (messageObject) {
+        const messageList = Object.keys(messageObject).map(key => ({
+          ...messageObject[key],
+          uid: key,
+        }));
+
+        this.setState({
+          messages: messageList,
+          loading: false,
+        });
+      } else {
+        this.setState({ messages: null, loading: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.messages().off();
+  }
+
+  render() {
+    const { messages, loading } = this.state;
+
+    return (
+      <div>
+        {loading && <div>Loading ...</div>}
+
+        {messages ? (
+          <MessageList messages={messages} />
+        ) : (
+          <div>There are no messages ...</div>
+        )}
+      </div>
+    );
+  }
+}
+
+const MessageList = ({ messages }) => (
+  <ul>
+    {messages.map(message => (
+      <MessageItem key={message.uid} message={message} />
+    ))}
+  </ul>
+);
+
+const MessageItem = ({ message }) => (
+  <li>
+    <strong>{message.userId}</strong> {message.text}
+  </li>
+);
+
+const Messages = withFirebase(MessageBase);
 
 const condition = authUser => !!authUser;
 
