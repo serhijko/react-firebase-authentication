7192d3954ce4e2b418739a0b776f2aaeef3b8871
serhijko <sp_by@tut.by>
2019-03-02 18:52:56 +0300

    Associate Entities in Firebase Realtime Database

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification,
+} from '../Session';
 import { withFirebase } from '../Firebase';
 
 const HomePage = () => (
@@ -54,9 +58,10 @@ class MessageBase extends Component {
     this.setState({ text: event.target.value });
   };
 
-  onCreateMessage = event => {
+  onCreateMessage = (event, authUser) => {
     this.props.firebase.messages().push({
       text: this.state.text,
+      userId: authUser.uid,
     });
 
     this.setState({ text: '' });
@@ -68,24 +73,28 @@ class MessageBase extends Component {
     const { text, messages, loading } = this.state;
 
     return (
-      <div>
-        {loading && <div>Loading ...</div>}
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            {loading && <div>Loading ...</div>}
 
-        {messages ? (
-          <MessageList messages={messages} />
-        ) : (
-          <div>There are no messages ...</div>
+            {messages ? (
+              <MessageList messages={messages} />
+            ) : (
+              <div>There are no messages ...</div>
+            )}
+
+            <form onSubmit={event => this.onCreateMessage(event, authUser)}>
+              <input
+                type="text"
+                value={text}
+                onChange={this.onChangeText}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
         )}
-
-        <form onSubmit={this.onCreateMessage}>
-          <input
-            type="text"
-            value={text}
-            onChange={this.onChangeText}
-          />
-          <button type="submit">Send</button>
-        </form>
-      </div>
+      </AuthUserContext.Consumer>
     );
   }
 }
